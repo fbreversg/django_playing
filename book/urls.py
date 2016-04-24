03857fdfcfcdc66f343344ec951a374c710c0351
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-24 20:34:49 +0200

    Inheritance templates.

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import patterns, include, url
-from views import hello, current_datetime, hours_ahead
+from django.conf.urls import patterns, url
+from views import hello, current_datetime, hours_ahead, current_datetime_render
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,10 +7,10 @@ from views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
     url(r'^hello/$', hello),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^time/$', current_datetime),
     url(r'^timeRender/$', current_datetime_render),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^$', hello)
+    url(r'^$', hello),
     # Examples:
     # url(r'^$', 'book.views.home', name='home'),
     # url(r'^book/', include('book.foo.urls')),
--- book/views.py
+++ book/views.py
@@ -4,8 +4,10 @@ from django.template import Context
 from django.http import Http404, HttpResponse
 import datetime
 
+
 def hello(request):
     return HttpResponse("Hello world")
+
 
 def current_datetime(request):
     now = datetime.datetime.now()
@@ -13,9 +15,11 @@ def current_datetime(request):
     html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
 
+
 def current_datetime_render(request):
     now = datetime.datetime.now()
-    return render(request, 'current_datetime.html', {'current_date' : now})
+    return render(request, 'current_datetime.html', {'current_date': now})
+
 
 def hours_ahead(request, offset):
     try:
@@ -23,5 +27,4 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body><html>" % (offset, dt)
-    return HttpResponse(html)
+    return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
