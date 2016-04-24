04fc18dce72160a2544304c64a2a5fbe559d9ef3
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-24 18:36:28 +0200

    initial commit

---
+++ book/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from book.views import hello, current_datetime
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^ime/plus/\(d{1,2})/$', hours_ahead),
+    url(r'^$', hello)
+    # Examples:
+    # url(r'^$', 'book.views.home', name='home'),
+    # url(r'^book/', include('book.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ book/views.py
@@ -0,0 +1,17 @@
+from django.http import Http404, HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body><html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
