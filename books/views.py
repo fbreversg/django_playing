0035986f111807f987914362362c28efb4e7686c
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-25 16:31:06 +0200

    Error handling.

--- books/views.py
+++ books/views.py
@@ -14,6 +14,4 @@ def search(request):
         books = Book.objects.filter(title__icontains=q)
         return render(request, 'search_results.html', {'books': books, 'query': q})
     else:
-        message = 'You submitted an empty form.'
-
-    return HttpResponse(message)
+        return render(request, 'search_form.html', {'error': True})
