538e565d10392f041e72103e4dfa810ec966993a
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-25 16:26:43 +0200

    GET result example

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from books.models import Book
 
 
 def search_form(request):
@@ -8,8 +9,10 @@ def search_form(request):
 
 def search(request):
 
-    if 'q' in request.GET:
-        message = 'You search for : %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render(request, 'search_results.html', {'books': books, 'query': q})
     else:
         message = 'You submitted an empty form.'
 
