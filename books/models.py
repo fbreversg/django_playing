f668bd4add8f2742a5b9e5385009402c7253c1e0
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-26 17:35:08 +0200

    Extra manager

--- books/models.py
+++ books/models.py
@@ -25,11 +25,19 @@ class Author(models.Model):
         return u'%s %s' % (self.first_name, self.last_name)
 
 
+# Extra manager
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField(blank=True, null=True)
+    num_pages = models.IntegerField(blank=True, null=True)
+    objects = BookManager  # extra manager example: Book.objects.title_count('django')
 
     def __unicode__(self):
         return self.title
