4945e97162b2b2f5d775bc34f580f65498ccaf58
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-26 11:19:37 +0200

    Replaced with django form

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,16 @@
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def contact(request):
-    errors = []
+    # errors = []
     if request.method == 'POST':
+        """
+
+        Validation form to be replaced
+
         if not request.POST.get('subject', ''):
             errors.append('Enter a subject.')
         if not request.POST.get('message', ''):
@@ -19,13 +24,21 @@ def contact(request):
                 request.POST.get('email', 'noreply@example.com'),
                 ['siteowner@example.com'],
             )
+        """
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com']
+            )
             return HttpResponseRedirect('/contact/thanks')
-    return render(request, 'contact_form.html', {
-        'errors': errors,
-        'subject': request.POST.get('subject', ''),
-        'message': request.POST.get('message', ''),
-        'email': request.POST.get('email', ''),
-    })
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact_form.html', {'form': form})
 
 
 def thanks(request):
