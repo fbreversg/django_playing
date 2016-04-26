1ae15bf57ce2d1199b8a34a1320553c180c80a1d
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-26 16:52:38 +0200

    Template tags

--- templating/templatetags/extras.py
+++ templating/templatetags/extras.py
@@ -1,10 +1,12 @@
 from django import template
+import datetime
 
 register = template.Library()
 
 """
 register.filter('cut', cut)
 register.filter('lower', lower)
+register.tag('current_time', do_current_time)
 """
 
 
@@ -19,3 +21,24 @@ def cut(value, arg):
 def lower(value): # Only one argument.
     """Converts a string into all lowercase"""
     return value.lower()
+
+
+# Template tags
+@register.tag(name="current_time") # Replaces register.tag('current_time', do_current_time)
+def do_current_time(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, format_string = token.split_contents()
+    except ValueError:
+        msg = '%r tag requires a single argument' % token.split_contents()[0]
+        raise template.TemplateSyntaxError(msg)
+    return CurrentTimeNode(format_string[1:-1])
+
+
+class CurrentTimeNode(template.Node):
+    def __init__(self, format_string):
+        self.format_string = str(format_string)
+
+    def render(self, context):
+        now = datetime.datetime.now()
+        return now.strftime(self.format_string)
