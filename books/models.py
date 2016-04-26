ebeb9fb35350a65d970e950e99088fa5741ae09c
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-26 17:47:33 +0200

    Model methods

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.localflavor.us.models import USStateField
 
 
 class Publisher(models.Model):
@@ -43,3 +44,45 @@ class Book(models.Model):
         return self.title
 
 
+# First, define the Manager subclass.
+class DahlBookManager(models.Manager):
+    def get_query_set(self):
+        return super(DahlBookManager, self).get_query_set().filter(author='Roald Dahl')
+
+
+# Then hook it into the Book model explicitly.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    # ...
+
+    objects = models.Manager() # The default manager.
+    dahl_objects = DahlBookManager() # The Dahl-specific manager.
+
+
+# Model methods
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = USStateField() # Yes, this is U.S.-centric...
+
+    def baby_boomer_status(self):
+        """Returns the person's baby-boomer status."""
+        import datetime
+        if datetime.date(1945, 8, 1) <= self.birth_date <= datetime.date(1964, 12, 31):
+            return "Baby boomer"
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        return "Post-boomer"
+
+    def is_midwestern(self):
+        """Returns True if this person is from the Midwest."""
+        return self.state in ('IL', 'WI', 'MI', 'IN', 'OH', 'IA', 'MO')
+
+    def _get_full_name(self):
+        """Returns the person's full name."""
+        return u'%s %s' % (self.first_name, self.last_name)
+    full_name = property(_get_full_name)
