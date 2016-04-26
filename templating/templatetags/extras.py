aedf644fb85d76df7334b5dce1e25fbba102acd2
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-26 16:38:20 +0200

    Template filters

---
+++ templating/templatetags/extras.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+"""
+register.filter('cut', cut)
+register.filter('lower', lower)
+"""
+
+
+# Filters
+@register.filter(name='cut') # Replaces register.filter('cut', cut)
+def cut(value, arg):
+    """Removes all values of arg from the given string"""
+    return value.replace(arg, '')
+
+
+@register.filter  # Replaces register.filter('lower', lower)
+def lower(value): # Only one argument.
+    """Converts a string into all lowercase"""
+    return value.lower()
